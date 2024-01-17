290fcd6f8c6b3a45369e24b7c19840585e376e42
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-01-17 01:22:46 +0000

    commiting FP 1st commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,28 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+
+CREATE TABLE "Food" (
+  "foodId" serial PRIMARY KEY,
+  "name" text,
+  "description" text,
+  "price" int,
+  "category" text
+);
+
+CREATE TABLE "Users" (
+  "userId" serial PRIMARY KEY,
+  "hashedPassword" text,
+  "username" text
+);
+
+CREATE TABLE "Carts" (
+  "userId" int,
+  "foodId" int,
+  PRIMARY KEY ("userId", "foodId")
+);
+
+ALTER TABLE "Carts" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Carts" ADD FOREIGN KEY ("foodId") REFERENCES "Food" ("foodId");
