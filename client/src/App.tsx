626fef7d83ca6f0a221c9cabd6ad2ea0ff77ee38
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-01-23 08:27:29 +0000

    commited PROJ

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './css/App.css';
+import { IntroScreen } from './components/IntroScreen';
 import { MenuPageWrap } from './components/MenuPageWrap';
 import { MenuDetails } from './components/MenuDetails';
 import { ChooseDrinks } from './components/ChooseDrink';
@@ -8,10 +9,16 @@ import { OrderConfirmation } from './components/Order_confirmation';
 import { SignUpForm } from './components/SignUp';
 import { SignInForm } from './components/SignIn';
 
+const tokenKey = 'react-context-jwt';
+
 export default function App() {
+  const [cartItems, setCartItems] = useState();
+
+  useEffect(() => {});
   return (
     <>
       <Routes>
+        <Route index element={<IntroScreen />} />
         <Route path="/" index element={<MenuPageWrap />} />
         <Route path="signIn/" element={<SignInForm />} />
         <Route path="signUp/" element={<SignUpForm />} />
