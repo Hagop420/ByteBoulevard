ec56b49c158a696fb5149c818a35ff1acc978b49
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-11-02 06:14:08 +0000

    aws-2

--- client/src/components/IntroScreen.tsx
+++ client/src/components/IntroScreen.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/introScreen.css';
+//
 
 export const IntroScreen = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'retro');
